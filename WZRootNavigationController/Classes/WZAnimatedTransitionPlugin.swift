15212c2767e3e1e90fab7fdc91a4978df9e61bb5
wu.zhe <wuzhezmc@gmail.com>
2018-04-25 10:52:38 +0800

    优化了转场动画

--- WZRootNavigationController/Classes/WZAnimatedTransitionPlugin.swift
+++ WZRootNavigationController/Classes/WZAnimatedTransitionPlugin.swift
@@ -11,6 +11,11 @@ open class WZAnimatedTransitionPlugin: NSObject {
     public let operation:UINavigationControllerOperation
     public fileprivate(set) weak var fromViewController:WZContainerController!
     public fileprivate(set) weak var toViewController:WZContainerController!
+    fileprivate lazy var maskView: UIView = {
+        let maskView = UIView(frame: UIScreen.main.bounds)
+        maskView.backgroundColor = UIColor.black
+        return maskView
+    }()
     @objc public required init(operation:UINavigationControllerOperation ,from fromViewController:WZContainerController, to toViewController:WZContainerController) {
         self.operation = operation
         self.fromViewController = fromViewController
@@ -21,61 +26,64 @@ open class WZAnimatedTransitionPlugin: NSObject {
 
 extension WZAnimatedTransitionPlugin:UIViewControllerAnimatedTransitioning{
     open func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
-        return 0.3
+        return 0.6
     }
     
     open func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
-        guard let from = transitionContext.viewController(forKey: .from) as? WZContainerController,
-            let to = transitionContext.viewController(forKey: .to) as? WZContainerController
+        guard var from = transitionContext.viewController(forKey: .from) as? WZContainerController,
+            var to = transitionContext.viewController(forKey: .to) as? WZContainerController
             else {
                 fatalError("transitionContext .from/.to 必须是 WZContainerController")
                 return
         }
-        
+        let containerView = transitionContext.containerView
         let duration = self.transitionDuration(using: transitionContext)
-        let containerViewColor = transitionContext.containerView.backgroundColor
-        if self.operation == .push {
-            transitionContext.containerView.backgroundColor = from.contentViewController.view.backgroundColor
-            transitionContext.containerView.addSubview(from.view)
-            let hudView = UIView(frame:transitionContext.containerView.bounds)
-            hudView.backgroundColor = UIColor(hue: 0.0, saturation: 0.0, brightness: 0.0, alpha: 0.4)
-            hudView.alpha = 0.1
-            transitionContext.containerView.addSubview(hudView)
-            let frame = transitionContext.finalFrame(for: to)
-            to.view.frame = frame.offsetBy(dx: frame.width, dy: 0.0)
-            transitionContext.containerView.addSubview(to.view)
-            UIView.animate(withDuration: duration, delay: 0.0, options:[.curveEaseOut], animations: {
-                from.view.transform = CGAffineTransform(scaleX: 0.97, y: 0.97)
-                hudView.alpha = 1.0
-                to.view.frame = frame
-                
-            }) { (finish) in
-                hudView.removeFromSuperview()
-                transitionContext.containerView.backgroundColor = containerViewColor
-                from.view.alpha = 1.0
-                from.view.transform = .identity
-                transitionContext.completeTransition(!transitionContext.transitionWasCancelled)
-            }
-        }else if self.operation == .pop {
-            transitionContext.containerView.backgroundColor = to.contentViewController.view.backgroundColor
-            let tabBarHidden = from.tabBarController?.tabBar.isHidden ?? true
-            from.tabBarController?.tabBar.isHidden = true
-            to.view.transform = CGAffineTransform(scaleX: 0.97, y: 0.97)
-            transitionContext.containerView.addSubview(to.view)
-            let hudView = UIView(frame:transitionContext.containerView.bounds)
-            hudView.backgroundColor = UIColor(hue: 0.0, saturation: 0.0, brightness: 0.0, alpha: 0.4)
-            hudView.alpha = 1.0
-            transitionContext.containerView.addSubview(hudView)
-            transitionContext.containerView.bringSubview(toFront: from.view)
-            UIView.animate(withDuration: duration, delay: 0.0, options:[.curveLinear], animations: {
-                to.view.transform = .identity
-                hudView.alpha = 0.0
-                from.view.frame = from.view.frame.offsetBy(dx: from.view.frame.width, dy: 0.0)
-            }) { (finished) in
-                hudView.removeFromSuperview()
-                transitionContext.containerView.backgroundColor = containerViewColor
-                to.tabBarController?.tabBar.isHidden = tabBarHidden
-                transitionContext.completeTransition(!transitionContext.transitionWasCancelled)
+        
+        var fromStartPx:CGFloat = 0.0
+        var fromEndPx:CGFloat = -UIScreen.main.bounds.width * 0.618 //视差设置
+        var toStartPx:CGFloat = UIScreen.main.bounds.width
+        var toEndPx:CGFloat = 0.0
+        var startOpacity:Float = 0.0
+        var endOpacity:Float = 0.3
+        
+        if self.operation == .pop {
+            swap(&from, &to)
+            swap(&fromStartPx, &fromEndPx)
+            swap(&toStartPx, &toEndPx)
+            swap(&startOpacity, &endOpacity)
+        }
+        
+        containerView.addSubview(from.view)
+        containerView.addSubview(to.view)
+        from.view.addSubview(maskView)
+        
+        maskView.layer.opacity = startOpacity
+        from.view.layer.position.x = fromStartPx + from.view.layer.bounds.width / 2.0
+        to.view.layer.position.x = toStartPx + to.view.layer.bounds.width / 2.0
+        let shadowOpacityBackup = to.view.layer.shadowOpacity
+        let shadowOffsetBackup = to.view.layer.shadowOffset
+        let shadowRadiusBackup = to.view.layer.shadowRadius
+        let shadowPathBackup = to.view.layer.shadowPath
+        to.view.layer.shadowOpacity = 0.5
+        to.view.layer.shadowOffset = CGSize(width: -3.0, height: 0.0)
+        to.view.layer.shadowRadius = 5.0
+        to.view.layer.shadowPath = CGPath(rect: to.view.layer.bounds, transform: nil)
+        
+        UIView.animate(withDuration: duration, delay: 0.0, options: .curveEaseInOut, animations: {
+            self.maskView.layer.opacity = endOpacity
+            from.view.layer.position.x = fromEndPx + from.view.layer.bounds.width / 2.0
+            to.view.layer.position.x = toEndPx + to.view.layer.bounds.width / 2.0
+        }) { finished in
+            transitionContext.completeTransition(!transitionContext.transitionWasCancelled)
+            self.maskView.removeFromSuperview()
+            if !transitionContext.transitionWasCancelled {
+                to.view.layer.shadowOpacity = 0.0
+                if finished {
+                    to.view.layer.shadowOpacity = shadowOpacityBackup
+                    to.view.layer.shadowOffset = shadowOffsetBackup
+                    to.view.layer.shadowRadius = shadowRadiusBackup
+                    to.view.layer.shadowPath = shadowPathBackup
+                }
             }
         }
     }
